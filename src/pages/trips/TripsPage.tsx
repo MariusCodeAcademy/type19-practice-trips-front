83c4cceb9bbd83579333c81c909845eb68b219b8
MariusMokymai <testams752@gmail.com>
2024-03-14 11:32:59 +0200

    get trips

--- src/pages/trips/TripsPage.tsx
+++ src/pages/trips/TripsPage.tsx
@@ -2,15 +2,29 @@
 
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { beBaseUrl } from '../../config';
+import { TripObjType } from '../../types/types';
 
 export default function TripsPage() {
-  const [tripsArr, setTripsArr] = useState(null);
+  const [tripsArr, setTripsArr] = useState<TripObjType[] | null>(null);
+  console.log('tripsArr ===', tripsArr);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    getPosts(`${beBaseUrl}/trips`).then((data) => setTripsArr(data));
+  }, []);
 
-  function getPosts() {
+  function getPosts(url: string): Promise<TripObjType[] | null> {
     // su axios gaunam postus ir irasom i tripsArr
-    // axios.get()
+    return axios
+      .get(url)
+      .then((resp) => {
+        // console.log('resp.data ===', resp.data);
+        return resp.data;
+      })
+      .catch((error: Error) => {
+        console.log('error ===', error);
+        return null;
+      });
   }
 
   return (
@@ -18,6 +32,13 @@ export default function TripsPage() {
       <div className='container'>
         <h1 className='display-2'>TripsPage</h1>
         <p>Welcome to our TripsPage</p>
+        <ul className='list-group'>
+          {tripsArr?.map((tObj) => (
+            <li className='list-group-item' key={tObj.id}>
+              {tObj.name}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
