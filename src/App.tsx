02b76cf0cd50c2dcb892a3d34d3543d10fc0b0f4
MariusMokymai <testams752@gmail.com>
2024-03-14 10:45:09 +0200

    routes to trips

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './styles/App.css';
 import Header from './components/layout/Header';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import TripsPage from './pages/trips/TripsPage';
+import SingleTripPage from './pages/trips/SingleTripPage';
 
 export default function App() {
   return (
@@ -10,6 +12,8 @@ export default function App() {
       <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/trips' element={<TripsPage />} />
+        <Route path='/trips/:tripId' element={<SingleTripPage />} />
       </Routes>
     </div>
   );
