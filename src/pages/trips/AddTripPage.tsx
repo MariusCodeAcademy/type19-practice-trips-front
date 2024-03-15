fa58550b661e9d59dc4694da1d99dde399b7d24b
MariusMokymai <testams752@gmail.com>
2024-03-15 09:20:57 +0200

    form page

---
+++ src/pages/trips/AddTripPage.tsx
@@ -0,0 +1,12 @@
+export default function AddTripPage() {
+  return (
+    <div>
+      <div className='container'>
+        <h1 className='display-2'>AddTripPage</h1>
+        <p>Welcome to our AddTripPage</p>
+
+        <form action=''></form>
+      </div>
+    </div>
+  );
+}
