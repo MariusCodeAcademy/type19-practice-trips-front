c509aad101450034690627fa75ba5e8609aa8157
MariusMokymai <testams752@gmail.com>
2024-03-15 10:33:35 +0200

    getNice date + single trip page

--- src/pages/trips/SingleTripPage.tsx
+++ src/pages/trips/SingleTripPage.tsx
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom';
 import { TripObjType } from '../../types/types';
 import { beBaseUrl } from '../../config';
 import axios from 'axios';
+import { getNiceDate } from '../../utils/helpers';
 
 type tripParam = {
   readonly tripId: string;
@@ -40,8 +41,25 @@ export default function SingleTripPage() {
   return (
     <div>
       <div className='container'>
-        <h1 className='display-2'>{currentTrip?.name}</h1>
-        <p className='lead'>{currentTrip?.date}</p>
+        <div className='left'>
+          <img
+            className='img-fluid'
+            src={'/img/' + currentTrip?.image_main}
+            alt={currentTrip?.name}
+          />
+        </div>
+        <div className='right'>
+          <h1 className='display-2'>{currentTrip?.name}</h1>
+          <p className='lead'>{getNiceDate(currentTrip?.date || '')}</p>
+          <p>
+            <span className='fw-bold'>{currentTrip?.country}</span> - {currentTrip?.city}
+          </p>
+          <p className=''>{currentTrip?.description}</p>
+          <div className='control d-flex gap-2'>
+            <button className='btn btn-secondary'>Go back</button>
+            <button className='btn btn-danger'>Delete</button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -4,6 +4,12 @@
 
 // getNiceDate(dateString)
 
-//export
+export function getNiceDate(dateString: string): string {
+  const dateObj = new Date(dateString);
+  const formatedDate = dateObj.toLocaleDateString('lt-LT', { dateStyle: 'medium' });
+  return formatedDate;
+}
+
+// export
 
 // panaudojam Single trip page
