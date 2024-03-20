cf9eb6be3db6fe6ccc1f9eff98c42056fcec858d
MariusMokymai <testams752@gmail.com>
2024-03-20 16:18:18 +0200

    better types

--- src/components/UI/InputEl.tsx
+++ src/components/UI/InputEl.tsx
@@ -1,42 +1,33 @@
 import { FormikProps } from 'formik';
-import { TripObjTypeNoId } from '../../types/types';
 
-type InputElProps = {
+type InputElProps<T> = {
   placeholder: string;
   type?: 'text' | 'email' | 'number' | 'date' | 'textarea' | 'password';
-  // id: keyof TripObjTypeNoId;
-  // formik: FormikProps<TripObjTypeNoId>;
-  id: string;
-  formik: any;
+  readonly id: keyof T;
+  formik: FormikProps<T>;
   className?: string;
 };
 
-export default function InputEl({
-  formik,
-  id,
-  placeholder,
-  className,
-  type = 'text',
-}: InputElProps) {
+export function InputEl<T>({ formik, id, placeholder, className, type = 'text' }: InputElProps<T>) {
   const Element = type === 'textarea' ? 'textarea' : 'input';
 
-  const isError = formik.errors && formik.errors[id] && formik.touched && formik.touched[id];
+  const isError = formik.errors[id] && formik.touched[id];
   return (
     <label className={`form-label w-100 ${className}`}>
       <span>{placeholder}</span>
       <Element
-        value={formik.values[id]}
+        value={formik.values[id] as string | number | readonly string[] | undefined}
         onChange={formik.handleChange}
         onBlur={formik.handleBlur}
         // isInvalid tik kai kalaida ir blur
         className={`form-control ${isError ? 'is-invalid' : ''}`}
         type={type}
-        id={id}
+        id={id.toString()}
         placeholder={placeholder}
       />
       {isError && (
         <span className='bg-danger-subtle rounded-1 d-block text-danger px-3 py-1 '>
-          {formik.errors[id]}
+          {formik.errors[id]?.toString()}
         </span>
       )}
     </label>
